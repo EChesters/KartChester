1496aea03132bed7c3346fc6b2c09f2d805f067a
3oheme <bohemegm@gmail.com>
2013-10-26 23:21:30 +0100

    @3oheme started adding countdown

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,23 @@
-var game = new Game(1);
-var frontend = new Frontend(game);
-game.init();
-setInterval(function(){game.update(frontend);},1000);
+
+function start_game() {
+	var game = new Game(1);
+	var frontend = new Frontend(game);
+	game.init();
+	setInterval(function(){game.update(frontend);},1000);
+}
+
+function start_countdown() {
+	window.setTimeout($('#countdown_1').show(), 0);
+
+	window.setTimeout($('#countdown_1').hide(), 1000);
+	window.setTimeout($('#countdown_2').show(), 1000);
+	
+	window.setTimeout($('#countdown_2').hide(), 1000);
+	window.setTimeout($('#countdown_3').show(), 2000);
+
+	window.setTimeout($('#countdown_3').show(), 3000);
+}
+
+
+window.setTimeout(start_countdown, 0);
+window.setTimeout(start_game, 3000);

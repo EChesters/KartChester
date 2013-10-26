672d910569851e2b9651feda725e29355c71c8ae
Daley Chetwynd <d_chetwynd@fastmail.co.uk>
2013-10-26 22:00:57 +0100

    Daley - Refactored move validator

--- js/move_validator.js
+++ js/move_validator.js
@@ -1,15 +1,8 @@
 function MoveValidator() {
   this.validate = function(player_location, target_segment, graph_wrapper) {
     connected_segments = graph_wrapper.get_connected_edges_ids(player_location.current_segment.id);
+    target_segment_position_in_connected_segments = connected_segments.indexOf(target_segment.id);
 
-    for (var index = 0; index < connected_segments.length; index++)
-    {
-      if (connected_segments[index] === target_segment.id)
-      {
-        return true;
-      }
-    }
-
-    return false;
+    return target_segment_position_in_connected_segments !== -1;
   }
 }

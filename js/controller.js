fba5f8088b7fbdb8bd08863b38f20c084b7de07a
3oheme <bohemegm@gmail.com>
2013-10-26 23:55:03 +0100

    @ignacio removed some shit

--- js/controller.js
+++ js/controller.js
@@ -6,18 +6,4 @@ function start_game() {
 	setInterval(function(){game.update(frontend);},1000);
 }
 
-function start_countdown() {
-	window.setTimeout($('#countdown_1').show(), 0);
-
-	window.setTimeout($('#countdown_1').hide(), 1000);
-	window.setTimeout($('#countdown_2').show(), 1000);
-	
-	window.setTimeout($('#countdown_2').hide(), 1000);
-	window.setTimeout($('#countdown_3').show(), 2000);
-
-	window.setTimeout($('#countdown_3').show(), 3000);
-}
-
-
-window.setTimeout(start_countdown, 0);
 window.setTimeout(start_game, 3000);

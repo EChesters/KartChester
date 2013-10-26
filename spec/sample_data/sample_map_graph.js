9d8e01455cf6e7425d7a541e409bf18e7e850a66
Daley Chetwynd <d_chetwynd@fastmail.co.uk>
2013-10-26 22:11:10 +0100

    Daley Mircea changed JSON IDs from strings to integers

--- spec/sample_data/sample_map_graph.js
+++ spec/sample_data/sample_map_graph.js
@@ -1,7 +1,7 @@
 var sample_data_graph =
 [
   {
-    "id": "1",
+    "id": 1,
     "start_x": 100,
     "start_y": 100,
     "end_x": 200,
@@ -9,7 +9,7 @@ var sample_data_graph =
     "connected_edges": [2]
   },
   {
-    "id": "2",
+    "id": 2,
     "start_x": 200,
     "start_y": 200,
     "end_x": 300,
@@ -17,7 +17,7 @@ var sample_data_graph =
     "connected_edges": [1,3]
   },
   {
-    "id": "3",
+    "id": 3,
     "start_x": 300,
     "start_y": 200,
     "end_x": 400,
@@ -25,7 +25,7 @@ var sample_data_graph =
     "connected_edges": [2,4]
   },
   {
-    "id": "4",
+    "id": 4,
     "start_x": 300,
     "start_y": 200,
     "end_x": 400,

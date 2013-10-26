3f47f96aed61d0894755bcafa3587ed2d73760c6
Mircea Moise <mmoise@thoughtworks.com>
2013-10-26 18:38:13 +0100

    Mircea : changing new segment to new Segment

--- js/segment_parser.js
+++ js/segment_parser.js
@@ -7,7 +7,7 @@ function segment_parser() {
       var start_point = new point(raw_map_segment["start_x"], raw_map_segment["start_y"]);
       var end_point = new point(raw_map_segment["end_x"], raw_map_segment["end_y"]);
 
-      segments.push(new segment(start_point, end_point));
+      segments.push(new Segment(start_point, end_point));
     }
     
     return segments;

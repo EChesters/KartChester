f0b50faefcf86945236e5fb48c6ea54e7e922b97
Elizabeth Chesters <elizabeth.chesters@gmail.com>
2013-10-26 21:05:17 +0100

    Elizabeth: Added temp js file to return accurate position
    Returns cursor position relative to the board. Takes the board size into
    account and returns a double between 0 and 100.

--- js/temp.js
+++ js/temp.js
@@ -2,8 +2,14 @@ $(document).ready(function (e) {
 
 	$('#board').click(function (e) {
 		var posX = ($(this).offset().left);
-		var	posY = $(this).offset().top;
-        console.log((e.pageX - posX) + ' , ' + (e.pageY - posY));
-    });
+		var boardWidth = ($(this).width()) / 100;
+		var finalX = (e.pageX - posX)/boardWidth;
 
+		var	posY = $(this).offset().top;
+		var boardHeight = ($(this).height()) / 100;
+		var finalY = (e.pageY - posY)/boardHeight;
+
+		console.log(boardHeight + ": " + boardWidth);
+        console.log( finalX + ' , ' + finalY);
+    });
 });
